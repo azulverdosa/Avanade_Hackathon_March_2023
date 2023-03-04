be5f2e56fc7f3381f2810ec0e01aefdb08368749
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-04 16:14:23 -0500

    create temp styles file

---
+++ src/components/styles.js
@@ -0,0 +1,112 @@
+// <Checkboxes label={'heart'} />
+
+//     <Checkbox
+//       // {...label}
+//       icon={<FavoriteBorder />}
+//       checkedIcon={<Favorite />}
+//       // checked={checked}
+//       // onChange={handleChange}
+//       // inputProps={{ 'aria-label': 'controlled' }}
+//     />
+//     <Checkbox
+//       // {...label}
+//       icon={<BookmarkBorderIcon />}
+//       label="End"
+//       labelPlacement="end"
+//       checkedIcon={<BookmarkIcon />}
+//     />
+
+//     <FormControlLabel value="end" control={<Checkbox />} label="End" labelPlacement="end" />
+
+//     <Box sx={{ display: 'flex' }}>
+//       <FormControl sx={{ m: 1 }} component="fieldset" variant="standard">
+//         <FormLabel component="legend">Create Report</FormLabel>
+//         <FormGroup>
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 // checked={gilad}
+//                 // onChange={handleChange}
+//                 name="age"
+//                 defaultChecked
+//                 color="primary"
+//               />
+//             }
+//             label="Age"
+//           />
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 // checked={jason}
+//                 // onChange={handleChange}
+//                 name="gender"
+//                 defaultChecked
+//                 color="secondary"
+//               />
+//             }
+//             label="Gender"
+//           />
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 // checked={antoine}
+//                 // onChange={handleChange}
+//                 name="seniority"
+//                 defaultChecked
+//                 color="success"
+//               />
+//             }
+//             label="Seniority (Years in Company)"
+//           />
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 // checked={antoine}
+//                 // onChange={handleChange}
+//                 name="salary"
+//                 defaultChecked
+//                 color="default"
+//               />
+//             }
+//             label="Yearly Salary (including Bonus)"
+//           />
+
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 // checked={antoine}
+//                 // onChange={handleChange}
+//                 name="promotions"
+//                 defaultChecked
+//                 sx={{
+//                   'color': teal[500],
+//                   '&.Mui-checked': {
+//                     color: teal[500],
+//                   },
+//                 }}
+//               />
+//             }
+//             label="Number of Promotions"
+//           />
+
+//           <FormControlLabel
+//             control={
+//               <Checkbox
+//                 // checked={antoine}
+//                 // onChange={handleChange}
+//                 name="range"
+//                 defaultChecked
+//                 sx={{
+//                   'color': pink[800],
+//                   '&.Mui-checked': {
+//                     color: pink[600],
+//                   },
+//                 }}
+//               />
+//             }
+//             label="Date Range"
+//           />
+//         </FormGroup>
+//         <FormHelperText>Be careful</FormHelperText>
+//       </FormControl>
+//     </Box>
