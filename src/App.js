2eb287ed8dfc30f0be1e707eb1ed92264fdf5b88
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-04 16:13:12 -0500

    remove unsed components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import Chart from './components/Chart';
 import Dashboard from './components/Dashboard';
-import NavBar from './components/NavBar';
 
 function App() {
   return (
     <main className="App">
-      {/* <header className="App-header">
-        <p>Welcome to your App 🙊 🙉 🙈</p>
-        <p>Avanade Hackathon 🎉</p>
-      </header> */}
       <Dashboard />
-      <div>
-        <NavBar />
-      </div>
     </main>
   );
 }
