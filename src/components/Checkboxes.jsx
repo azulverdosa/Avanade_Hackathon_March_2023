1727593fac8e03881bdc82c23966895e8ec9b602
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-04 13:13:35 -0500

    create checkboxes component to test options for UI

---
+++ src/components/Checkboxes.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import Checkbox from '@mui/material/Checkbox';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+import Favorite from '@mui/icons-material/Favorite';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import BookmarkIcon from '@mui/icons-material/Bookmark';
+
+// const label = 'heart';
+// { inputProps: { 'aria-label': 'Checkbox demo' } };
+
+export default function IconCheckboxes({ label }) {
+  return (
+    <div>
+      <Checkbox label={label} icon={<FavoriteBorder />} checkedIcon={<Favorite />} />
+      <Checkbox {...label} icon={<BookmarkBorderIcon />} checkedIcon={<BookmarkIcon />} />
+    </div>
+  );
+}
