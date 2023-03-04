1ec455e30b9bf0a92c3ef3de2e3fc6caeb1bffe9
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-04 16:14:07 -0500

    change mock data

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -13,11 +13,11 @@ function createData(id, date, name, shipTo, paymentMethod, amount) {
 }
 
 const rows = [
-  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
-  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
-  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
+  createData(0, 'Elvis Presley', 45, 'Male', '•••• 3719', 36182),
+  createData(1, 'Ella Fitzgerald', 39, 'Female', '•••• 2574', 86686),
+  createData(2, 'Beyonce', 26, 'Female', '•••• 1253', 16080),
+  createData(3, 'Cindy Lauper', 31, 'Female', '•••• 2000', 66584),
+  createData(4, 'Bruce Springsteen', 57, 'Male', '•••• 5919', 26182),
 ];
 
 function preventDefault(event) {
@@ -27,15 +27,15 @@ function preventDefault(event) {
 export default function Orders() {
   return (
     <React.Fragment>
-      <Title>Recent Orders</Title>
+      <Title>Employee Information</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
             <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Age</TableCell>
+            <TableCell>Gender</TableCell>
+            <TableCell>Salary</TableCell>
+            <TableCell align="right"> Details</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -44,14 +44,14 @@ export default function Orders() {
               <TableCell>{row.date}</TableCell>
               <TableCell>{row.name}</TableCell>
               <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell>{`$${row.amount}`}</TableCell>
+              <TableCell align="right">{row.paymentMethod}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
       <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        See more orders
+        See more
       </Link>
     </React.Fragment>
   );
