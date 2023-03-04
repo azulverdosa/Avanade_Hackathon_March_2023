2fe501c64fcaecd312bbb8f00cebfb2358f95b62
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-04 02:49:32 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Chart from './components/Chart';
+import Dashboard from './components/Dashboard';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      {/* <header className="App-header">
+        <p>Welcome to your App 🙊 🙉 🙈</p>
+        <p>Avanade Hackathon 🎉</p>
+      </header> */}
+      <Dashboard />
+      <div>
+        <NavBar />
+      </div>
+    </main>
   );
 }
 
