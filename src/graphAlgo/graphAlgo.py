2459626c9ffb1c5634b9527da6910c100580778e
Emilie Z <emilieyzh@gmail.com>
2023-03-05 18:21:09 -0500

    Completed 2 more graph algorithms

--- src/graphAlgo/graphAlgo.py
+++ src/graphAlgo/graphAlgo.py
@@ -5,8 +5,22 @@ import numpy as np
 # Graphs to make:
 
 # 1. age, gender, salary, role
+"""
+role: each role in a different colour
+gender: female in dot, male in box points
+age: age in the x axis
+salary: salary in the y axis
 
-# 2. gender, salary, length of service (seniority)
+"""
+
+# 2. gender, average salary, role
+"""
+Bar graph
+gender in two colours
+average salary for each role
+
+"""
+
 
 # receive a request successfully for this test case:
 # 3. female population over the years
@@ -45,7 +59,144 @@ ax.set_title("Female Percentage in Workplace (" + str(first_year) + " - " + str(
 # this save the graph into a png file at specified path
 plt.savefig("./src/images/FemalePercentageOverYearsGraph.png")
 
+#################################################
+#GRAPH 2. gender, average salary, role #
+#################################################
 
+df2 = pd.read_csv('./src/graphAlgo/EmployeeDataSet.csv')
+
+# extract the year column of the csv file
+# df is a dictionary
+# df_list turns dictionary into list of tuples
+df2_list = list(df.items()) 
+    
+#Sales & Marketing
+sales_list_f = []
+sales_list_m = []
+
+#Operations
+operations_list_f = []
+operations_list_m = []
+
+#Finance
+finance_list_f = []
+finance_list_m = []
+
+#Procurement
+proc_list_f = []
+proc_list_m = []
+
+#Analytics
+analytics_list_f = []
+analytics_list_m = []
+
+#Technology
+tech_list_f = []
+tech_list_m = []
+
+#HR
+hr_list_f = []
+hr_list_m = []
+
+# make a graph of year vs fprofessionals
+fig2, ax2 = plt.subplots(figsize=(10, 7))
+for i in range(1, len(df2)):
+    if df2["gender"][i] == 'm':
+        if df2["department"][i] == "Sales & Marketing":
+            sales_list_m.append(df2["salary"][i])
+        elif df2["department"][i] == "Operations":
+            operations_list_m.append(df2["salary"][i])
+        elif df2["department"][i] == "Finance":
+            finance_list_m.append(df2["salary"][i])
+        elif df2["department"][i] == "Procurement":
+            proc_list_m.append(df2["salary"][i])
+        elif df2["department"][i] == "Analytics":
+            analytics_list_m.append(df2["salary"][i])
+        elif df2["department"][i] == "HR":
+            hr_list_m.append(df2["salary"][i])
+        else:
+            tech_list_m.append(df2["salary"][i])
+    elif df2["gender"][i] == 'f':
+        if df2["department"][i] == "Sales & Marketing":
+            sales_list_f.append(df2["salary"][i])
+        elif df2["department"][i] == "Operations":
+            operations_list_f.append(df2["salary"][i])
+        elif df2["department"][i] == "Finance":
+            finance_list_f.append(df2["salary"][i])
+        elif df2["department"][i] == "Procurement":
+            proc_list_f.append(df2["salary"][i])
+        elif df2["department"][i] == "Analytics":
+            analytics_list_f.append(df2["salary"][i])
+        elif df2["department"][i] == "HR":
+            hr_list_f.append(df2["salary"][i])
+        else:
+            tech_list_f.append(df2["salary"][i])
+
+
+# averages of each sector
+
+# male averages
+male_means = []
+male_means.append(sum(sales_list_m) / len(sales_list_m))
+male_means.append(sum(operations_list_m) / len(operations_list_m))
+male_means.append(sum(finance_list_m) / len(finance_list_m))
+male_means.append(sum(proc_list_m) / len(proc_list_m))
+male_means.append(sum(analytics_list_m) / len(analytics_list_m))
+male_means.append(sum(hr_list_m) / len(hr_list_m))
+male_means.append(sum(tech_list_m) / len(tech_list_m))
+
+# female averages
+female_means = []
+female_means.append(sum(sales_list_f) / len(sales_list_f))
+female_means.append(sum(operations_list_f) / len(operations_list_f))
+female_means.append(sum(finance_list_f) / len(finance_list_f))
+female_means.append(sum(proc_list_f) / len(proc_list_f))
+female_means.append(sum(analytics_list_f) / len(analytics_list_f))
+female_means.append(sum(hr_list_f) / len(hr_list_f))
+female_means.append(sum(tech_list_f) / len(tech_list_f))
+
+subjects = ['Sales & Marketing', 'Operations', 'Finance', 'Procurement', 'Analytics', 'HR', 'Technology']
+
+indx = np.arange(len(subjects))
+score_label = np.arange(0, 400000, 50000)
+
+bar_width = 0.35
+
+# labelling graph 
+barMale = ax2.bar(indx - bar_width/2, male_means, bar_width, label='Male means')
+barFemale = ax2.bar(indx + bar_width/2, female_means, bar_width, label='Female means')
+ax2.set_xlabel('Department Sectors', fontsize=14)
+ax2.set_ylabel('Average Yearly Salary Range ($)', fontsize=14)
+ax2.set_title("Salary per Department", fontsize=14)
+
+# inserting x axis label
+ax2.set_xticks(indx)
+ax2.set_xticklabels(subjects)
+
+# inserting y axis label
+ax2.set_yticks(score_label)
+ax2.set_yticklabels(score_label)
+
+# inserting legend
+ax2.legend()
+
+def insert_data_labels(bars):
+	for bar in bars:
+		bar_height = bar.get_height()
+		ax2.annotate('{0:.0f}'.format(bar.get_height()),
+			xy=(bar.get_x() + bar.get_width() / 2, bar_height),
+			xytext=(0, 3),
+			textcoords='offset points',
+			ha='center',
+			va='bottom'
+		)
+
+insert_data_labels(barMale)
+insert_data_labels(barFemale)
+
+plt.savefig("./src/images/SalaryGenderRole.png")
+
+"""
 #################################################
 #SAMPLE GRAPH. Work life balance over seniority #
 #################################################
@@ -104,6 +255,4 @@ text = txt1 + txt1stat + txt2 + txt2stat
 plt.figtext(0.5, 0.01, text , wrap=True, horizontalalignment='center', fontsize=12)
 plt.subplots_adjust(bottom = 0.18)
 plt.show()
-
-# Graph 3: female vs male counts in different sectors
-
+"""
