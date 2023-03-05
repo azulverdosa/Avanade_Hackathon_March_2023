bdceecf89898f713373a0161a62bff607d9d43cc
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-05 15:58:59 -0500

    change layout

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import Title from './Title';
 
 import useQueryFilters from '../customHooks/useQueryFilters';
-import { Box, Link } from '@mui/material';
+import { Box, Link, Paper } from '@mui/material';
 import EmployeeInformation from './EmployeeInformation';
 import NextSteps from './NextSteps';
 import SaveIcon from '@mui/icons-material/Save';
@@ -17,22 +17,31 @@ export default function Chart() {
 
   return (
     queryResults && (
-      <Box style={{ display: 'flex', flexDirection: 'column' }}>
-        <Title>Your Results</Title>
-        <Box style={{ display: 'flex' }}>
-          <img style={{ height: 'auto', width: '100%' }} alt="graph" src={queryResults} />
+      <Paper
+        sx={{
+          p: 2,
+          display: 'flex',
+          flexDirection: 'column',
+          height: 'auto',
+        }}
+      >
+        <Box style={{ display: 'flex', flexDirection: 'column' }}>
+          <Title>Your Results</Title>
+          <Box style={{ display: 'flex' }}>
+            <img style={{ height: 'auto', width: '100%' }} alt="graph" src={queryResults} />
+          </Box>
+          <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
+            Save Results
+            {/* <SaveIcon /> */}
+          </Link>
+          <Box style={{ display: 'flex', flexDirection: 'column' }} sx={{ mt: 3 }}>
+            <EmployeeInformation />
+          </Box>
+          <Box style={{ display: 'flex', flexDirection: 'column' }} sx={{ mt: 3 }}>
+            <NextSteps />
+          </Box>
         </Box>
-        <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-          Save Results
-          {/* <SaveIcon /> */}
-        </Link>
-        <Box style={{ display: 'flex', flexDirection: 'column' }} sx={{ mt: 3 }}>
-          <EmployeeInformation />
-        </Box>
-        <Box style={{ display: 'flex', flexDirection: 'column' }} sx={{ mt: 3 }}>
-          <NextSteps />
-        </Box>
-      </Box>
+      </Paper>
     )
   );
 }
