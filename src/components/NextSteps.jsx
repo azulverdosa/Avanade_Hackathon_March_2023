7a22a660c963a90e73d0611df791244faea17c02
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-05 21:58:59 -0500

    add styling

--- src/components/NextSteps.jsx
+++ src/components/NextSteps.jsx
@@ -15,26 +15,29 @@ export default function NextSteps() {
 
         <ol>
           <li>
-            Implement gender neutral recruitment processes Carefully word your job adverts. Research
-            shows that adjectives such as 'competitive'' and 'determined'' put off women. On the
-            other hand, words such as 'collaborative'' and 'cooperative'' tend to attract more women
-            than men. Standardise interviews, anonymise resumes and use blind evaluation processes.
-            Companies have found that blind evaluation procedures — including work sample tests and
-            neuroscientific tests of an applicant''s aptitude and skills — have helped them recruit
-            from more diverse backgrounds.{' '}
+            <b>Implement gender neutral recruitment processes</b>
+            <br />
+            Carefully word your job adverts. Researchshows that adjectives such as 'competitive''
+            and 'determined'' put off women. On the other hand, words such as 'collaborative'' and
+            'cooperative'' tend to attract more women than men. Standardise interviews, anonymise
+            resumes and use blind evaluation processes. Companies have found that blind evaluation
+            procedures — including work sample tests and neuroscientific tests of an applicant''s
+            aptitude and skills — have helped them recruit from more diverse backgrounds.
           </li>
           <br />
           <li>
-            Review salaries and standardise pay Frequently review salaries for parity between
-            genders and races. When recruiting, set the pay range offered based on years' experience
-            with some leeway for special achievements, not on how well the candidate negotiated
-            their last pay package.
+            <b>Review salaries and standardise pay </b>
+            <br />
+            Frequently review salaries for parity between genders and races. When recruiting, set
+            the pay range offered based on years' experience with some leeway for special
+            achievements, not on how well the candidate negotiated their last pay package.
           </li>
           <br />
           <li>
-            Provide training on unconscious bias Educate employees about their own unconscious bias.
-            Although this does not guarantee that attitudes will change, it does help employees to
-            understand their biases and to work towards eliminating them.
+            <b>Provide training on unconscious bias</b> <br />
+            Educate employees about their own unconscious bias. Although this does not guarantee
+            that attitudes will change, it does help employees to understand their biases and to
+            work towards eliminating them.
           </li>
         </ol>
 
