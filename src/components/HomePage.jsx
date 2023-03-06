62606273f97a6364a85c4231b7b9464df31d50b2
Ava Torre <91709114+azulverdosa@users.noreply.github.com>
2023-03-06 01:17:50 -0500

    code cleanup

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,7 @@
 import * as React from 'react';
-import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
-import Title from './Title';
 import { Box } from '@mui/system';
 import { Paper } from '@mui/material';
-
-function preventDefault(event) {
-  event.preventDefault();
-}
 
 export default function Highlights({ show }) {
   return (
